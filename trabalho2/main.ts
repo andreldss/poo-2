8266fce2d62840841a9417a2b3cb3c5303ed9077
andreldss <110506132+andreldss@users.noreply.github.com>
2023-05-08 23:14:12 -0300

    Create main.ts

---
+++ trabalho2/main.ts
@@ -0,0 +1,11 @@
+import { Carro } from "./carro";
+import { Entrada } from "./entrada";
+
+let Ka = new Carro("ABC123A2", 0);
+console.log(Ka)
+let Km1 = new Entrada(Ka, 50, 5, 25);
+console.log(Ka)
+let Km2 = new Entrada(Ka, 90, 5, 25);
+console.log(Ka)
+
+
